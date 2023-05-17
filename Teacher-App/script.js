090ffa7e5c8c88293f874f79c7269cac264d90fd
IJslap <45030475+ijslapje@users.noreply.github.com>
2023-05-17 17:42:14 +0200

    basic structure

---
+++ Teacher-App/script.js
@@ -0,0 +1,29 @@
+let gameStart = false
+let teamAmt
+let startTime
+let locationData = JSONdata
+
+function getData(){
+    //get the data from the frontend
+    return teamAmt
+}
+
+
+function calcTeams(){
+    //Read Json
+    
+    //Give every team (teamAmt) a different order array of locations
+}
+
+function getTime(){
+    //get currentTime
+}
+
+function sendData(){
+    //send the data to frontend
+    startTime = getTime()
+    teamData = calcTeams()
+    gameStart = true
+}
+
+console.log(sendData())
